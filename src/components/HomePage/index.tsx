8481075bfcdb8c3f6897aaee3209ab99a3183df0
smiskus <saramiskus@gmail.com>
2025-04-03 19:57:16 -0500

    Homepage cleanup

--- src/components/HomePage/index.tsx
+++ src/components/HomePage/index.tsx
@@ -1,23 +1,14 @@
 import "./homepage.scss";
-import { Header } from "../Header";
 import { LatestExperiencesCarousel } from "./components/LatestExperiencesCarousel";
-import { FavoritedExperiencesCarousel } from "./components/FavoritedRestaurantsCarousel";
-import axios from "axios";
-import { useQuery } from "react-query";
-
-
 
 export const HomePage = () => {
   // Make profile call
   // Make latest restaurant call
   // Make saved restaurants call
 
-
-
   return (
     <div className="home-container">
       <LatestExperiencesCarousel />
-      <FavoritedExperiencesCarousel />
     </div>
   );
 };
